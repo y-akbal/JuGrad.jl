e5dc7f945b2062c7f95c038dffd1a875a57b06bb
Yıldırım Akbal <yildirim.akbal@gmail.com>
2024-02-17 11:47:49 +0300

    Examples

--- src/diff_functions.jl
+++ src/diff_functions.jl
@@ -2,6 +2,7 @@
 mutable struct diff_f <: Function
     f_::Function
     grad::Function
+    def::String
 end
 
 function (f::diff_f)(x::T) where T 
@@ -26,14 +27,14 @@ end
 
 ## activation functions, and their derivatives!!!!
 ## Use here  @inline and @fastmath kind a stuff
-relu_ = diff_f(x->max(x,0), x-> ifelse(x>0, one(x), zero(x)))
-leakyrelu_ = diff_f(x->max(x,-0.1*x), x-> ifelse(x>0, one(x), -0.1*one(x)))
-sigmoid_ = diff_f(x->1/(1+exp(-x)), x -> exp(x)/(1+exp(x))^2)
-tanh_ = diff_f(x->(exp(x) - exp(-x))/(exp(x) + exp(-x)), x -> one(x)-((exp(x) - exp(-x))/(exp(x) + exp(-x)))^2)
-kelu_ = diff_f(x->ifelse(x > 0, x*(1-exp(-x)/2), x*exp(x)/2), x-> ifelse(x > 0, (1-exp(-x)/2)*(1-x*exp(-x)/2), (x+1)*exp(x)/2))
-log_ = diff_f(x->log(x), x->1/x)
-sum_ = diff_f(x->sum(x), x->zero(x).+1)
-ID = diff_f(x->x, x->one(x))
+relu_ = diff_f(x->max(x,0), x-> ifelse(x>0, one(x), zero(x)),"Relu")
+leakyrelu_ = diff_f(x->max(x,-0.1*x), x-> ifelse(x>0, one(x), -0.1*one(x)),"LeakyRelu")
+sigmoid_ = diff_f(x->1/(1+exp(-x)), x -> exp(x)/(1+exp(x))^2, "Sigmoid")
+tanh_ = diff_f(x->(exp(x) - exp(-x))/(exp(x) + exp(-x)), x -> one(x)-((exp(x) - exp(-x))/(exp(x) + exp(-x)))^2, "Tanh")
+kelu_ = diff_f(x->ifelse(x > 0, x*(1-exp(-x)/2), x*exp(x)/2), x-> ifelse(x > 0, (1-exp(-x)/2)*(1-x*exp(-x)/2), (x+1)*exp(x)/2), "Kelu")
+log_ = diff_f(x->log(x), x->1/x, "log_")
+sum_ = diff_f(x->sum(x), x->zero(x).+1, "sum")
+ID = diff_f(x->x, x->one(x), "ID")
 ##
 ## Simple functions will be here like sum norm mean loss vs vs....
 ## 

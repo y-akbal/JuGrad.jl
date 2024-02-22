4643f853409e5148e5a0b5e2615e7c3953e59d73
Yıldırım Akbal <yildirim.akbal@gmail.com>
2024-02-22 11:14:20 +0300

    binary classification is done!

--- examples/nn/MLP_binary_classifier.jl
+++ examples/nn/MLP_binary_classifier.jl
@@ -3,13 +3,13 @@ using JuGrad.nn:Linear
 using Random
 using StatsBase
 using PyCall
+
 @pyimport sklearn.datasets as dataset
 @pyimport numpy as np
 
 
 
 function return_dataset(n_samples = 100; seed = 0)
-
     X_train, y_train = dataset.make_circles(n_samples=n_samples, random_state = seed)
     X_test, y_test = dataset.make_circles(n_samples=n_samples, random_state = seed+1)
     return map(x->np.transpose(x), [X_train, y_train, X_test, y_test])
@@ -17,45 +17,51 @@ end
 
 
 @kwdef mutable struct sequential_binary <: JuGrad.nn.AbstractNeuralNetwork
-    lay1 = Linear(2, 10; σ = JuGrad.leakyrelu_)
-    lay2 = Linear(10, 10; σ = JuGrad.leakyrelu_)
-    lay3 = Linear(10, 1) ## 
+    lay1 = Linear(2, 10; σ = JuGrad.tanh_)
+    lay2 = Linear(10, 1)
 end
 
 function (seq::sequential_binary)(x)
-    return seq.lay3(seq.lay2(seq.lay1(x)))
+    return seq.lay2(seq.lay1(x))
 end
 
 function loss_from_logits(y_pred, y_true)
-    return  -mean(@. (1-y_true)*JuGrad.log_(JuGrad.sigmoid_(1- y_pred)) + (y_true)*JuGrad.log_(JuGrad.sigmoid_(y_pred)))
+    y_true = y_true |> transpose ## Remember that Julia is column-major!!!<ssss
+    return -mean( @. (1-y_true)*JuGrad.log_(1-JuGrad.sigmoid_(y_pred)) + (y_true)*JuGrad.log_(JuGrad.sigmoid_(y_pred)))
 end
-"""
-X_train, y_train, X_test, y_test = return_dataset()
-network = sequential_binary()
-network(X_test)
-"""
+
+
 function main()
-    optimizer = JuGrad.nn.Descent(0.000000000000000000001)
+    Random.seed!(0)
+
+    optimizer = JuGrad.nn.Descent(0.01)
     X_train, y_train, X_test, y_test = return_dataset()
+
     @info "Data Collected!!!"
     network = sequential_binary()
     @info "Network Created, the training is about to start!!!"
+    acc::Float64 = 0.0
 
-    for i in 1:1000
+    for i in 1:10000
         loss_ = loss_from_logits(network(X_train),y_train) ## Calculate the loss!!!
-        @info "loss is $(loss_)"
+        if i%100 == 0
+            @info "loss is $(loss_) and accuracy $(acc)"
+        end
+
         loss_.grad = 1 ## This is kinda must, we can eliminate; though the code would be pretty ugly and hard to read!!!
         backward!(loss_) ## We accumulated the gradients in a backwards manner now!!!
         JuGrad.nn.step!(optimizer, network)
+
+        acc = (1*(map(x->Float64(x.w), network(X_test)) .> 0) .== y_test |> transpose .|> Int64) |> mean
         zero_grad!(loss_)
+
     end
+
 end
 
-
-
-
-main()
-## Put a real dataset here!!!
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
 
 
 

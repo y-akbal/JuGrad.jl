01a2e7311cd7d29ed32bf9e34971684513e5ff5d
y-akbal <yildirim.akbal@gmail.com>
2024-02-24 00:25:15 +0300

    Regression problem is ok

--- examples/nn/MLP_binary_classifier.jl
+++ examples/nn/MLP_binary_classifier.jl
@@ -10,12 +10,30 @@ using PyCall
 
 
 function return_dataset(n_samples = 100; seed = 0)
-    X_train, y_train = dataset.make_circles(n_samples=n_samples, random_state = seed)
-    X_test, y_test = dataset.make_circles(n_samples=n_samples, random_state = seed+1)
+    X_train, y_train = dataset.make_moons(n_samples=n_samples, random_state = seed)
+    X_test, y_test = dataset.make_moons(n_samples=n_samples, random_state = seed+1)
+    X_train, X_test = map(x->x+0.1*randn(x|>size), [X_train, X_test])
     return map(x->np.transpose(x), [X_train, y_train, X_test, y_test])
 end
 
+"""X_train, y_train, X_test, y_test = return_dataset()
 
+using Plots
+X_train = X_train |> transpose
+scatter(X_train[y_train .== 1, 1], X_train[y_train .== 1, 2], label = "Label-1")
+scatter!(X_train[y_train .== 0, 1], X_train[y_train .== 0, 2], label = "Label-0")
+using IterTools
+p = product(1:10,1:10)
+f.(p)
+network = sequential_binary()
+product(1:10, 1:10)
+for i in -1:0.1:1
+    for j in -1:0.1:1
+        scatter!([i j])
+
+    end
+end
+"""
 @kwdef mutable struct sequential_binary <: JuGrad.nn.AbstractNeuralNetwork
     lay1 = Linear(2, 10; σ = JuGrad.tanh_)
     lay2 = Linear(10, 1)
@@ -24,6 +42,8 @@ end
 function (seq::sequential_binary)(x)
     return seq.lay2(seq.lay1(x))
 end
+
+seq_prediction(X) = map(x->Float64(x.w), network(X |> transpose)) |> transpose
 
 function loss_from_logits(y_pred, y_true)
     y_true = y_true |> transpose ## Remember that Julia is column-major!!!<ssss
